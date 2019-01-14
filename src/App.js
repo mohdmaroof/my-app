89ccdca4af48ae62b4b88b436b434c149c8afd25
maroof <maroof@gmail.com>
2019-01-14 17:53:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Hello</h2>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
